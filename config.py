402d01b8bd62d1017cedcb53c4762766a67d87bb
Jonathan Davenport <j.davenport@elsevier.com>
2019-07-04 10:13:06 -0400

    updates comment

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 
 class Config(object):
-  # this key is required by flask to run - however as this app does not have users per se
-  # this key is not actually relevant to the functioning or security of the app
+  # this key is required by flask to run in order to encrypt session data -
+  # however as this app does not have users per se
+  # it is not actually relevant to the security of the app, and thus
+  # commiting it is not a security risk at this time
   SECRET_KEY = os.environ.get('JIRA_GRAPH_VIZ_KEY') or '2mdfYZRmyFf5EyziWghe'
